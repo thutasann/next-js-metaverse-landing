71accd9c057b8ed2ef48686a5f7919b5a3c38fd5
thutasann <thutasann@gmail.com>
2022-11-28 16:55:40 +0630

    `Feedback` and `Footer`

--- utils/motion.js
+++ utils/motion.js
@@ -124,3 +124,41 @@ export const planetVariants = (direction) => ({
         },
     },
 });
+
+export const zoomIn = (delay, duration) => ({
+    hidden:{
+        scale: 0,
+        opacity: 0
+    },
+    show:{
+        scale: 1,
+        opacity: 1,
+        transition: {
+            type: 'tween',
+            delay,
+            duration,
+            ease: 'easeOut'
+        },
+    },
+});
+
+export const footerVariants = {
+    hidden: {
+        opacity: 0,
+        y: 50,
+        transition: {
+            type: 'spring',
+            stiffness: 300,
+            damping: 140,
+        },
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'spring',
+            stiffness: 80,
+            delay: 0.5,
+        },
+    },
+};
