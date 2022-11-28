e721e41787a92e14ab7b7d3e7be5929a0c9fc340
thutasann <thutasann@gmail.com>
2022-11-28 16:18:29 +0630

    `World` and `Insight` section

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,9 +1,48 @@
 'use client';
 
-const InsightCard = () => (
-  <div>
-    Insight Card
-  </div>
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import { fadeIn } from '../utils/motion';
+
+const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+  <motion.div
+    variants={fadeIn('up', 'spring', index * 0.5, 1)}
+    className="flex flex-col gap-4 md:flex-row"
+  >
+    <Image
+      src={imgUrl}
+      width={270}
+      height={250}
+      priority
+      quality={100}
+      alt="planet-01"
+      className="object-cover rounded-md"
+    />
+    <div className="flex items-center justify-between w-full">
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
+        <h4 className="font-normal lg:text-[42px] text-[26px] text-white">
+          {title}
+        </h4>
+        <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">
+          {subtitle}
+        </p>
+      </div>
+
+      <div
+        className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white"
+      >
+        <img
+          src="/arrow.svg"
+          width={40}
+          height={40}
+          priority
+          quality={100}
+          alt="arrow"
+          className="object-contain"
+        />
+      </div>
+    </div>
+  </motion.div>
 );
 
 export default InsightCard;
--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,79 @@
 'use client';
 
+import { motion } from "framer-motion";
+import styles from '../styles';
+import Image from "next/image";
+import { TitleText, TypingText  } from '../components';
+import { staggerContainer, fadeIn } from '../utils/motion'
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}
+      >
+        <TypingText title="| People on the World"  textStyles="text-center" />
+        <TitleText
+          title="Track friends around you and invite them to play together in the same world"
+          textStyles="text-center"
+        />
+
+        <motion.div
+          variants={fadeIn('up', 'tween', 0.3, 1)}
+          className="relative mt-[68px] flex w-full h-[550px]"
+        >
+          <Image
+            width={3000}
+            height={500}
+            priority
+            quality={100}
+            src="/map.png"
+            alt="MAP"
+            className="object-cover"
+          />
+
+          {/* Places */}
+          <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+            <Image
+              src="/people-01.png"
+              alt="People 01"
+              width={70}
+              height={70}
+              loading="lazy"
+              blurDataURL="/people-01.png"
+            />
+          </div>
+
+          {/* Places */}
+          <div className="absolute top-0 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+            <Image
+              src="/people-02.png"
+              alt="People 02"
+              width={70}
+              height={70}
+              loading="lazy"
+              blurDataURL="/people-03.png"
+            />
+          </div>
+
+          {/* Places */}
+          <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+            <Image
+              src="/people-03.png"
+              alt="People 03"
+              width={70}
+              height={70}
+              loading="lazy"
+              blurDataURL="/people-03.png"
+            />
+          </div>
+
+
+        </motion.div>
+      </motion.div>
   </section>
 );
 
