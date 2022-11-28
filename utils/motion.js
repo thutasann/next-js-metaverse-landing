18fc10af23cf0229320004f12e4903892582d6c4
thutasann <thutasann@gmail.com>
2022-11-28 15:30:35 +0630

    `Get Started1

--- utils/motion.js
+++ utils/motion.js
@@ -108,3 +108,19 @@ export const textVariant2 = {
         },
     },
 };
+
+export const planetVariants = (direction) => ({
+    hidden:{
+        x: direction === "left" ? "-100%" : "100%",
+        rotate: 120
+    },
+    show:{
+        x: 0,
+        rotate: 0,
+        transition:{
+            type: 'spring',
+            duration: 1.8,
+            delay: 0.5
+        },
+    },
+});
